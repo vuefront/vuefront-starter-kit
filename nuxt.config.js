5971baafe10e0a3990d095aed7e467e5467b95a6
alexandrdrvn <php4.drvn@gmail.com>
2019-12-11 17:07:42 +0200

    fix: update template

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,42 +2,43 @@ require('dotenv').config()
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 
 export default {
-    mode: 'universal',
-    env: {
-        FEATURED_PRODUCT: process.env.FEATURED_PRODUCT
-    },
-    generate: {
-        // concurrency: 20,
-        subFolders: false
-    },
-    head: {
-        title: 'vuefront',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'VueFront' }
-        ],
-        link: [{
-            rel: 'icon',
-            type: 'image/png',
-            href: '/favicon.ico'
-        }],
-        script: []
-    },
-    loading: { color: '#3B8070' },
-    modules: [
-        '@nuxtjs/pwa',
-        '@nuxtjs/dotenv',
-        'vuefront-nuxt',
-        'cookie-universal-nuxt'
+  mode: 'universal',
+  env: {
+    FEATURED_PRODUCT: process.env.FEATURED_PRODUCT
+  },
+  generate: {
+    concurrency: 1,
+    subFolders: false
+  },
+  head: {
+    title: 'vuefront',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'VueFront' }
     ],
-    build: {
-        babel: {
-            plugins: ['lodash']
-        },
-        extractCSS: true,
-        plugins: [new LodashModuleReplacementPlugin({
-            shorthands: true
-        })]
-    }
+    link: [{
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon.ico'
+      }],
+    script: []
+  },
+  loading: { color: '#3B8070' },
+  modules: [
+    '@nuxtjs/pwa',
+    '@nuxtjs/dotenv',
+    'vuefront-nuxt',
+    'cookie-universal-nuxt'
+  ],
+  build: {
+    publicPath: process.env.SITE_URL + '/_nuxt/',
+    babel: {
+      plugins: ['lodash']
+    },
+    extractCSS: true,
+    plugins: [new LodashModuleReplacementPlugin({
+        shorthands: true
+      })]
+  }
 }
--- vuefront.config.js
+++ vuefront.config.js
@@ -1,103 +1,103 @@
 export default {
-    logo: require('~/assets/img/logo_header.svg'),
-    footerLogo: require('~/assets/img/logo_footer.svg'),
-    layouts: {
-        '*': {
-            headerMenu: [
-                [
-                    'Menu',
-                    {
-                        items: ['store', 'blog']
-                    }
-                ]
-            ],
-            footerLeft: ['Pages'],
-            footerCenter: ['AccountLinks'],
-            footerRight: [
-                [
-                    'ExtraLinks',
-                    {
-                        links: [{
-                                to: '/store/special',
-                                text: 'Special'
-                            },
-                            {
-                                to: '/store/compare',
-                                text: 'Compare'
-                            },
-                            {
-                                to: '/contact',
-                                text: 'Contact Us'
-                            }
-                        ]
-                    }
-                ]
+  logo: require('~/assets/img/logo_header.svg'),
+  footerLogo: require('~/assets/img/logo_footer.svg'),
+  layouts: {
+    '*': {
+      headerMenu: [
+        [
+          'Menu',
+          {
+            items: ['store', 'blog']
+          }
+        ]
+      ],
+      footerLeft: ['Pages'],
+      footerCenter: ['AccountLinks'],
+      footerRight: [
+        [
+          'ExtraLinks',
+          {
+            links: [{
+                to: '/store/special',
+                text: 'Special'
+              },
+              {
+                to: '/store/compare',
+                text: 'Compare'
+              },
+              {
+                to: '/contact',
+                text: 'Contact Us'
+              }
             ]
-        },
-        '/': {
-            contentTop: [
-                [
-                    'Slideshow',
-                    {
-                        items: [
-                            'https://opencart.vuefront.com/image/cache/catalog/demo/banners/MacBookAir-1140x380.jpg',
-                            'https://opencart.vuefront.com/image/cache/catalog/demo/banners/iPhone6-1140x380.jpg'
-                        ]
-                    }
-                ],
-                [
-                    'FeaturedProduct',
-                    {
-                        ids: JSON.parse(process.env.FEATURED_PRODUCT)
-                    }
-                ],
-                'LatestProduct',
-                'SpecialProduct',
-                'LatestPost'
+          }
+        ]
+      ]
+    },
+    '/': {
+      contentTop: [
+        [
+          'Slideshow',
+          {
+            items: [
+              'https://img.dreamvention.com/vuefront/banners/Banner_demo_1.jpg',
+              'https://img.dreamvention.com/vuefront/banners/Banner_demo_2.jpg'
             ]
-        },
-        '/search/*': {
-            contentBottom: ['SearchProduct', 'SearchPost']
-        },
-        '/store/category*': {
-            columnLeft: [
-                'StoreCategory', [
-                    'LatestProduct',
-                    {
-                        column: true
-                    }
-                ]
-            ]
-        },
-        '/blog/category*': {
-            columnRight: [
-                'Search',
-                'BlogCategory', [
-                    'LatestPost',
-                    {
-                        column: true
-                    }
-                ]
-            ]
-        },
-        '/store/product/*': {
-            contentBottom: ['RelatedProduct']
-        },
-        '/blog/post*': {
-            columnRight: [
-                [
-                    'LatestPost',
-                    {
-                        column: true
-                    }
-                ]
-            ]
-        },
-        '/account*': {
-            columnRight: ['Account']
-        },
-        '/store/checkout': {
-            contentTop: ['Checkout']
-        }
+          }
+        ],
+        [
+          'FeaturedProduct',
+          {
+            ids: JSON.parse(process.env.FEATURED_PRODUCT)
+          }
+        ],
+        'LatestProduct',
+        'SpecialProduct',
+        'LatestPost'
+      ]
+    },
+    '/search/*': {
+      contentBottom: ['SearchProduct', 'SearchPost']
+    },
+    '/store/category*': {
+      columnLeft: [
+        'StoreCategory', [
+          'LatestProduct',
+          {
+            column: true
+          }
+        ]
+      ]
+    },
+    '/blog/category*': {
+      columnRight: [
+        'Search',
+        'BlogCategory', [
+          'LatestPost',
+          {
+            column: true
+          }
+        ]
+      ]
+    },
+    '/store/product/*': {
+      contentBottom: ['RelatedProduct']
+    },
+    '/blog/post*': {
+      columnRight: [
+        [
+          'LatestPost',
+          {
+            column: true
+          }
+        ]
+      ]
+    },
+    '/account*': {
+      columnRight: ['Account']
+    },
+    '/store/checkout': {
+      contentTop: ['Checkout']
     }
+  }
 }
