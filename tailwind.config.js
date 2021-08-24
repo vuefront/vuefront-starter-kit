2378edd1cb094d74fb93b72138be2886098f2da0
Aleksandr Krasnyi <aleksandr.krasnyi@dreamvention.com>
2021-08-24 09:56:14 +0300

    fix: fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-    purge: [],
+    purge: {
+      content: [
+      './components/**/*.vue',
+      './node_module/vuefront/**/*.vue',
+      './node_module/@vuefront/checkout-app/**/*.vue',
+      ],
+      safelist: [/^vf-/]
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
       extend: {},
